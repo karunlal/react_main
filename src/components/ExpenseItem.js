968e0ddbd5e3c415a165c4a4886ea27aec4dff5e
Karunlal <karunlalp@gmail.com>
2023-10-22 16:04:36 +0530

    props started

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,17 @@
-function ExpenseItem(){
-    return<h2>Expense item!</h2>;
+import './ExpenseItem.css';
+function ExpenseItem(props){
+    const expenseDate = new Date(2021, 2,28);
+    const expenseTitle='Car Insurance';
+    const expenseAmount = 294.67;
+    return(
+        <div className="expense-item">
+            <div>{props.date.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </div>
+    );
     
 }
 
